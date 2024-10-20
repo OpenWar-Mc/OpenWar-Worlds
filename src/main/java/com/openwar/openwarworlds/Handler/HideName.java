9c3bc5c0a2ca9f22841e1b9531adfc8db1b2f43e
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-10-20 09:59:46 +0200

    Update HideName.java

--- src/main/java/com/openwar/openwarworlds/Handler/HideName.java
+++ src/main/java/com/openwar/openwarworlds/Handler/HideName.java
@@ -1,4 +1,48 @@
 package com.openwar.openwarworlds.Handler;
 
-public class HideName {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class HideName implements Listener {
+
+    private final Scoreboard scoreboard;
+
+    public HideName(JavaPlugin plugin) {
+        this.scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        checkPlayerWorld(player);
+    }
+
+    @EventHandler
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        checkPlayerWorld(player);
+    }
+
+    private void checkPlayerWorld(Player player) {
+        Team team = scoreboard.getTeam("hidden_" + player.getName());
+
+        if (team == null) {
+            team = scoreboard.registerNewTeam("hidden_" + player.getName());
+            team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+        }
+
+        if (player.getWorld().getName().equalsIgnoreCase("warzone")) {
+            team.addEntry(player.getName());
+        } else {
+            team.removeEntry(player.getName());
+            team.unregister();
+        }
+    }
 }
