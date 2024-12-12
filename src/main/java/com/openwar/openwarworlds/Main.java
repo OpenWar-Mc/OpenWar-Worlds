566b4e3e65aec915a8238ca602fb981207a26c81
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-12-12 15:56:52 +0100

    Update Main.java

--- src/main/java/com/openwar/openwarworlds/Main.java
+++ src/main/java/com/openwar/openwarworlds/Main.java
@@ -1,9 +1,7 @@
 package com.openwar.openwarworlds;
 
-import com.openwar.openwarfaction.commands.FactionTabCompletion;
+import com.openwar.openwarcore.Utils.LevelSaveAndLoadBDD;
 import com.openwar.openwarfaction.factions.FactionManager;
-import com.openwar.openwarfaction.handler.MenuHandler;
-import com.openwar.openwarlevels.level.PlayerDataManager;
 import com.openwar.openwarworlds.Commands.*;
 import com.openwar.openwarworlds.GUI.GUIbuild;
 import com.openwar.openwarworlds.Handler.ChangeWorldEvent;
@@ -11,7 +9,6 @@ import com.openwar.openwarworlds.Handler.GUIHandler;
 import com.openwar.openwarworlds.Handler.HideName;
 import com.openwar.openwarworlds.utils.LoaderSaver;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -20,14 +17,14 @@ import java.util.List;
 
 public final class Main extends JavaPlugin {
 
-    PlayerDataManager pl;
+    LevelSaveAndLoadBDD pl;
     FactionManager fm;
     GUIbuild gui;
     LoaderSaver ls;
     public List<Player> waitingPlayers = new ArrayList<>();
 
     private boolean setupDepend() {
-        RegisteredServiceProvider<PlayerDataManager> levelProvider = getServer().getServicesManager().getRegistration(PlayerDataManager.class);
+        RegisteredServiceProvider< LevelSaveAndLoadBDD> levelProvider = getServer().getServicesManager().getRegistration( LevelSaveAndLoadBDD.class);
         RegisteredServiceProvider<FactionManager> factionDataProvider = getServer().getServicesManager().getRegistration(FactionManager.class);
         if (levelProvider == null || factionDataProvider == null) {
             System.out.println("ERROR !!!!!!!!!!!!!!!!!!!!");
