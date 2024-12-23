c21cc7ddc752c6fb87705de2a2b7c989c6e93d9c
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-12-23 21:11:33 +0100

    Update HideName.java

--- src/main/java/com/openwar/openwarworlds/Handler/HideName.java
+++ src/main/java/com/openwar/openwarworlds/Handler/HideName.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
@@ -22,6 +23,7 @@ public class HideName implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         checkPlayerWorld(player);
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tablist reload");
     }
 
     @EventHandler
@@ -40,9 +42,15 @@ public class HideName implements Listener {
 
         if (player.getWorld().getName().equalsIgnoreCase("warzone")) {
             team.addEntry(player.getName());
+            System.out.println("JOUEUR "+player+ " A LE PSEUDO CACHAYYYY");
         } else {
             team.removeEntry(player.getName());
-            team.unregister();
+            System.out.println("JOUEUR "+player+ " A LE PSEUDO VISIBLEEE");
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tablist reload");
     }
 }
